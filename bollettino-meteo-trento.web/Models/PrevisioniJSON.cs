2afe0cbf86e3db474d18561fa8a91a4daa8d9572
macmat01 <matteo.machella01@gmail.com>
2024-09-13 22:11:43 +0200

    Add SOAP service for weather forecast
    Introduced `IMeteoSoapService` and `MeteoSoapService` to provide SOAP-based weather forecasts. Updated `PrevisioniJSON` models with DataContract and DataMember attributes for serialization. Added necessary package references and configured SOAP endpoint in `Program.cs`.

--- bollettino-meteo-trento.web/Models/PrevisioniJSON.cs
+++ bollettino-meteo-trento.web/Models/PrevisioniJSON.cs
@@ -1,58 +1,63 @@
-﻿namespace bollettino_meteo_trento.web.Models;
+﻿#region
 
+using System.Runtime.Serialization;
+
+#endregion
+namespace bollettino_meteo_trento.web.Models;
+
+[DataContract]
 public record RootObject(
-    string fonte_da_citare,
-    string codice_ipa_titolare,
-    string nome_titolare,
-    string codice_ipa_editore,
-    string nome_editore,
-    string dataPubblicazione,
-    int idPrevisione,
-    string evoluzione,
-    string evoluzioneBreve,
-    object[] AllerteList,
-    Previsione[] previsione
+    [property: DataMember] string fonte_da_citare,
+    [property: DataMember] string codice_ipa_titolare,
+    [property: DataMember] string nome_titolare,
+    [property: DataMember] string codice_ipa_editore,
+    [property: DataMember] string nome_editore,
+    [property: DataMember] string dataPubblicazione,
+    [property: DataMember] int idPrevisione,
+    [property: DataMember] string evoluzione,
+    [property: DataMember] string evoluzioneBreve,
+    [property: DataMember] object[] AllerteList,
+    [property: DataMember] Previsione[] previsione
 );
-
+[DataContract]
 public record Previsione(
-    string localita,
-    int quota,
-    Giorni[] giorni
+    [property: DataMember] string localita,
+    [property: DataMember] int quota,
+    [property: DataMember] Giorni[] giorni
 );
-
+[DataContract]
 public record Giorni(
-    int idPrevisioneGiorno,
-    string giorno,
-    int idIcona,
-    string icona,
-    string descIcona,
-    string testoGiorno,
-    int tMinGiorno,
-    int tMaxGiorno,
-    Fasce[] fasce
+    [property: DataMember] int idPrevisioneGiorno,
+    [property: DataMember] string giorno,
+    [property: DataMember] int idIcona,
+    [property: DataMember] string icona,
+    [property: DataMember] string descIcona,
+    [property: DataMember] string testoGiorno,
+    [property: DataMember] int tMinGiorno,
+    [property: DataMember] int tMaxGiorno,
+    [property: DataMember] Fasce[] fasce
 );
-
+[DataContract]
 public record Fasce(
-    int idPrevisioneFascia,
-    string fascia,
-    string fasciaPer,
-    string fasciaOre,
-    string icona,
-    string descIcona,
-    string idPrecProb,
-    string descPrecProb,
-    string idPrecInten,
-    string descPrecInten,
-    string idTempProb,
-    string descTempProb,
-    string idVentoIntQuota,
-    string descVentoIntQuota,
-    string idVentoDirQuota,
-    string descVentoDirQuota,
-    string idVentoIntValle,
-    string descVentoIntValle,
-    string idVentoDirValle,
-    string descVentoDirValle,
-    string iconaVentoQuota,
-    int zeroTermico
+    [property: DataMember] int idPrevisioneFascia,
+    [property: DataMember] string fascia,
+    [property: DataMember] string fasciaPer,
+    [property: DataMember] string fasciaOre,
+    [property: DataMember] string icona,
+    [property: DataMember] string descIcona,
+    [property: DataMember] string idPrecProb,
+    [property: DataMember] string descPrecProb,
+    [property: DataMember] string idPrecInten,
+    [property: DataMember] string descPrecInten,
+    [property: DataMember] string idTempProb,
+    [property: DataMember] string descTempProb,
+    [property: DataMember] string idVentoIntQuota,
+    [property: DataMember] string descVentoIntQuota,
+    [property: DataMember] string idVentoDirQuota,
+    [property: DataMember] string descVentoDirQuota,
+    [property: DataMember] string idVentoIntValle,
+    [property: DataMember] string descVentoIntValle,
+    [property: DataMember] string idVentoDirValle,
+    [property: DataMember] string iconaVentoQuota,
+    [property: DataMember] int zeroTermico
 );
