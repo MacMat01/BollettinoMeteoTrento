00add6a6bb672dd5fcde935a6b4e91f4ff7bfa43
Davide Pulvirenti <davidepulvirenti2013@gmail.com>
2024-09-29 00:31:54 +0200

    fix: auto generated id and timestamp ef conflict

--- BollettinoMeteoTrento.Api/Migrations/20240928222437_WithTimezone.Designer.cs
+++ BollettinoMeteoTrento.Api/Migrations/20240928222437_WithTimezone.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BollettinoMeteoTrento.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BollettinoMeteoTrento.Api.Migrations
 {
     [DbContext(typeof(PostgresContext))]
-    partial class PostgresContextModelSnapshot : ModelSnapshot
+    [Migration("20240928222437_WithTimezone")]
+    partial class WithTimezone
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
